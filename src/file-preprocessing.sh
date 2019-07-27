aa9c78cfd1bcbfa5b63c8ecff7cf8d4720bb8ca7
zmwebdev@gmail.com <zmwebdev@gmail.com>
2019-07-27 21:47:46 +0200

    file-preprocessing

--- src/file-preprocessing.sh
+++ src/file-preprocessing.sh
@@ -11,7 +11,7 @@ for FILE in $( find ${WIKIDIR} -name "wiki_*" ); do
 done
 
 # Concat all text files in each text directory.
-for DIR in $( find ${CURDIR} -mindepth 1 -type d ); do
+for DIR in $( find ${WIKIDIR} -mindepth 1 -type d ); do
     echo "Processing ${DIR}"
     for f in $( find ${DIR} -name "wiki_*" ); do cat $f >> ${DIR}/all.txt; done 
 done
