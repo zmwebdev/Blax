5ec223523334d0f51817e2c29072ce6792243ee3
zmwebdev@gmail.com <zmwebdev@gmail.com>
2019-07-27 21:07:28 +0200

    file-preprocessing.sh remove DOC and concat wiki files

---
+++ src/file-preprocessing.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Read data_text_dir path from a config file.
+CURDIR=$(cd $(dirname $0); pwd)
+
+# Text preprocessing.
+# Remove <doc id ... line and its next line (title of an article).
+for FILE in $( find ${CURDIR} -name "wiki_*" ); do
+    echo "Processing ${FILE}"
+    sed -i -e '/^$/d; /<doc id/,+1d; s/<\/doc>//g' ${FILE}
+done
+
+# Concat all text files in each text directory.
+for DIR in $( find ${CURDIR} -mindepth 1 -type d ); do
+    echo "Processing ${DIR}"
+    for f in $( find ${DIR} -name "wiki_*" ); do cat $f >> ${DIR}/all.txt; done 
+done
