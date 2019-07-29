07f3cd4e64ed8dc0e5275b0d579a89d60cd2a843
zmwebdev <zmwebdev@gmail.com>
2019-07-29 13:56:36 +0200

    remove <doc </doc

--- src/file-preprocessing.sh
+++ src/file-preprocessing.sh
@@ -7,11 +7,12 @@ WIKIDIR=$1
 # Remove <doc id ... line and its next line (title of an article).
 for FILE in $( find ${WIKIDIR} -name "wiki_*" ); do
     echo "Processing ${FILE}"
-    sed -i -e '/^$/d; /<doc id/,+1d; s/<\/doc>//g' ${FILE}
+    #sed -i -e '/^$/d; /<doc id/,+1d; s/<\/doc>//g' ${FILE}
+    sed -i -e '/<doc id/d; /<\/doc>/d' ${FILE}
 done
 
-# Concat all text files in each text directory.
-for DIR in $( find ${WIKIDIR} -mindepth 1 -type d ); do
-    echo "Processing ${DIR}"
-    for f in $( find ${DIR} -name "wiki_*" ); do cat $f >> ${DIR}/all.txt; done 
-done
+## Concat all text files in each text directory.
+#for DIR in $( find ${WIKIDIR} -mindepth 1 -type d ); do
+#    echo "Processing ${DIR}"
+#    for f in $( find ${DIR} -name "wiki_*" ); do cat $f >> ${DIR}/all.txt; done 
+#done
